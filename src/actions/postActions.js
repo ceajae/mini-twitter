09bb563a999cb931f04e84e3de1ae9d3894a4d9f
Chijioke <chijiokemolokwu@gmail.com>
2017-09-20 17:35:56 +0100

    make http request from client

--- src/actions/postActions.js
+++ src/actions/postActions.js
@@ -1,6 +1,14 @@
 import{ADD_POST, DELETE_POST} from '../constants/actionTypes';
 
 export function addPost(text,postId,userId,timeStamp){
+  if(!postId){
+      const post = JSON.parse(text)
+      text = post.text;
+      postId= post._id;
+      userId= post.userId;
+      timeStamp=post.timeStamp;
+  }
+
   return{
     type:ADD_POST,
     payload:{
--- src/views/Templates/Main/index.js
+++ src/views/Templates/Main/index.js
@@ -50,24 +50,40 @@ import Modal from '../../../components/Modal';
     }
 
     function _handleSubmitTweet(){
-            function generateId(){
-              const time =  Date.now().toString().split('');
-              let randomizer, letter, id='';
+        function generateId(){
+            const time =  Date.now().toString().split('');
+            let randomizer, letter, id='';
 
-              const alphaCodex=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-            //  const numCodex=['0','1','2','3','4','5','6','7','8','9'];
+            const alphaCodex=['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+          //  const numCodex=['0','1','2','3','4','5','6','7','8','9'];
 
-              for(var i=0; i<time.length; i++){
-                randomizer = Math.floor(Math.random()*10);
-                letter = alphaCodex[ Math.floor((alphaCodex.length * randomizer)/10)];
-              //  console.log(letter);
-                id+=letter;
-              }
-
-               return id;
-
+            for(var i=0; i<time.length; i++){
+              randomizer = Math.floor(Math.random()*10);
+              letter = alphaCodex[ Math.floor((alphaCodex.length * randomizer)/10)];
+            //  console.log(letter);
+              id+=letter;
             }
-        props.postTweet(props.tweetValue, generateId());
+
+             return id;
+
+        }
+
+        const stringPost = JSON.stringify({
+          text:props.tweetValue,
+          userId:generateId()
+        });
+
+        const request = new XMLHttpRequest();
+        request.open('POST', 'http://localhost:3030/tweet', true);
+        request.addEventListener('load', ()=>{
+          const response = request.responseText;
+          console.log(response);
+          props.postTweet(response);
+        })
+        request.send(stringPost);
+
+
+
         props.toggleModalVisibility(false);
 
     }
